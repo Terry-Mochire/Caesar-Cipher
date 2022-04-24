3de62c87d95ec2eeda2e76219abf4e890daf90e7
Terry <terrybmochire@gmail.com>
2022-04-25 01:56:32 +0300

    Write function to decipher user input.

--- src/main/java/org/example/Decoding.java
+++ src/main/java/org/example/Decoding.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class Decoding {
+    public static Encoding encoding = new Encoding();
+    private String userText;
+
+    public void decipher(String userText) {
+        this.userText = encoding.getPlaintext();
+        System.out.println("Decrypted String: " + userText);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,12 @@ package org.example;
 
 public class Main {
     public static Encoding encoding = new Encoding();
+    public static Decoding decoding = new Decoding();
     public static void main(String[] args) {
 
         encoding.userInput();
         System.out.println("Input String: " + encoding.getPlaintext());
         System.out.println("Encrypted String: " + encoding.cipher());
+        decoding.decipher(encoding.getPlaintext());
     }
 }
