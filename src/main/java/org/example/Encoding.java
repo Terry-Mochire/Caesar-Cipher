8c4f7aa0bf4503ae95b26ce408624af662e8c020
Terry <terrybmochire@gmail.com>
2022-04-24 21:03:20 +0300

    Collect cipher key from user, create array list of alphabets

--- src/main/java/org/example/Encoding.java
+++ src/main/java/org/example/Encoding.java
@@ -2,6 +2,8 @@ package org.example;
 
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import  java.util.List;
 public class Encoding {
     private   String plaintext = "";
 
@@ -13,6 +15,8 @@ public class Encoding {
         return plaintext;
     }
 
+    public Integer cipherKey;
+
     public void  userInput(){
         String cipherText = "";
 
@@ -22,11 +26,28 @@ public class Encoding {
             BufferedReader bufferedReader = new BufferedReader(streamReader);
             String plaintext = bufferedReader.readLine();
             this.setPlaintext(plaintext);
-
+            System.out.println("Enter your key: ");
+            String cipherKey = bufferedReader.readLine();
+            this.cipherKey = Integer.parseInt(cipherKey);
 
         }catch ( Exception e){
             e.printStackTrace();
         }
 
     }
+
+    public String cipher(){
+        String[] alphabets = {
+                "A","B","C","D","E","F","G",
+                "H","I","J","K","L","M","N",
+                "O","P","Q","R","S","T","U",
+                "V","W","X","Y","Z"
+        };
+
+        List<String> myList = new ArrayList<String>(List.of(alphabets));
+
+        System.out.println(this.cipherKey);
+        return "";
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static Encoding encoding = new Encoding();
     public static void main(String[] args) {
 
-       encoding.setPlaintext("The path to heaven begins in Hell");
         System.out.println(encoding.getPlaintext());
         encoding.userInput();
+
+        encoding.cipher();
     }
 }
