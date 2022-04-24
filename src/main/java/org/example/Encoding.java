7678082c4606e98666ca9bfccec5f8a859a564f6
Terry <terrybmochire@gmail.com>
2022-04-24 14:42:58 +0300

    Add getter and setter method to encoding class.

--- src/main/java/org/example/Encoding.java
+++ src/main/java/org/example/Encoding.java
@@ -3,16 +3,30 @@ package org.example;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 public class Encoding {
-    public static void main(String[] args) {
-       try {
-           System.out.println("Enter Plaintext: ");
-           InputStreamReader streamReader = new InputStreamReader(System.in);
-           BufferedReader bufferedReader = new BufferedReader(streamReader);
-           String plaintext = bufferedReader.readLine();
-           System.out.print("Your plaintext is " + plaintext);
-       } catch ( Exception e){
-           e.printStackTrace();
-       }
+    private   String plaintext = "";
+
+    public void setPlaintext(String plaintext){
+        this.plaintext = plaintext;
+    }
+
+    public String getPlaintext() {
+        return plaintext;
+    }
+
+    public void  userInput(){
+        String cipherText = "";
+
+        try{
+            System.out.print("Enter Plaintext: ");
+            InputStreamReader streamReader = new InputStreamReader(System.in);
+            BufferedReader bufferedReader = new BufferedReader(streamReader);
+            String plaintext = bufferedReader.readLine();
+            this.setPlaintext(plaintext);
+
+
+        }catch ( Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
+    public static Encoding encoding = new Encoding();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       encoding.setPlaintext("The path to heaven begins in Hell");
+        System.out.println(encoding.getPlaintext());
+        encoding.userInput();
     }
 }
